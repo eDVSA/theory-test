590b245897a6fd4f508dda75595f1e4bbeb408d7
eDVSA <108122794+eDVSA@users.noreply.github.com>
2022-10-14 09:55:53 +0100

    r1

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Book a driving test',
+  serviceName: 'Book a theory test',
 
   // Default port that prototype runs on
   port: '3000',
