55539bc8f4336d01934455fd7b6177d3a56e5dc3
eDVSA <108122794+eDVSA@users.noreply.github.com>
2022-10-14 11:07:21 +0100

    r1

--- app/routes.js
+++ app/routes.js
@@ -3,7 +3,5 @@ const router = express.Router()
 
 // Add your routes here - above the module.exports line
 
-require('./views/signin/v1/signin_validation')(router)
-require('./views/signin/v1/code_validation')(router)
 
 module.exports = router
